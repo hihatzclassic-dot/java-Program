b15236a488b19c70ba4b3116c678554c21e8102a
hihatzclassic-dot <hihatzclassic@gmail.com>
2026-04-08 14:53:08 +0100

    Computeloan

--- ComputeLoan.java
+++ ComputeLoan.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
 public class ComputeLoan {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter annual interest rate");
+    try (Scanner input = new Scanner(System.in)) {
+        System.out.print("Enter annual interest rate");
 
-    // Enter annual interest rate in percentage,
-    double annualInterestRate = input.nextDouble();
+        // Enter annual interest rate in percentage,
+        double annualInterestRate = input.nextDouble();
 
-    // Obtain monthly interest rate
-    double monthlyInterestRate = annualInterestRate / 1200;
+        // Obtain monthly interest rate
+        double monthlyInterestRate = annualInterestRate / 1200;
 
-    // Enter number of years
-    System.out.print("Enter number of years as an integer");
-    int numberOfYears = input.nextInt();
+        // Enter number of years
+        System.out.print("Enter number of years as an integer");
+        int numberOfYears = input.nextInt();
 
-    // Enter loan amount
-    System.out.print("Enter laon amount");
-    double loanAmount = input.nextDouble();
+        // Enter loan amount
+        System.out.print("Enter laon amount");
+        double loanAmount = input.nextDouble();
 
-    // Calculate payment
-    double monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears  * 12));
-    double totalPayment = monthlyPayment * numberOfYears * 12;
+        // Calculate payment
+        double monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears  * 12));
+        double totalPayment = monthlyPayment * numberOfYears * 12;
 
-    // Display results
-    System.out.println("The monthly payment is $" + (int)(monthlyPayment * 100) / 100.0);
-    System.out.println("The total payment is $" + (int)(totalPayment * 100) / 100.0);
+        // Display results
+        System.out.println("The monthly payment is $" + (int)(monthlyPayment * 100) / 100.0);
+        System.out.println("The total payment is $" + (int)(totalPayment * 100) / 100.0);
+    }
     }
 }
